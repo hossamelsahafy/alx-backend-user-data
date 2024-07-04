2294f332cbe100e97c57e3958e45bdc3b6438774
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-04 22:24:30 +0300

    File

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Regex-ing
+"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List, redaction: str, message: str,
+                 separator: str) -> str:
+    """
+        use a regex to replace occurrences of certain field values.
+    """
+    pattern = f"({'|'.join(fields)})=([^{separator}]*)"
+    return re.sub(pattern, lambda m: F"{m.group(1)}={redaction}", message)
