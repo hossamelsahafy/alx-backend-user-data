29c052b34080762f6f4d33bda42cb299d55f977c
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-05 00:11:11 +0300

    File

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,11 +5,67 @@
 import re
 from typing import List
 import logging
-import os
+from os import environ
 import mysql.connector
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """
+        use a regex to replace occurrences of certain field values.
+    """
+    pattern = f"({'|'.join(fields)})=([^{separator}]*)"
+    return re.sub(pattern, lambda m: F"{m.group(1)}={redaction}", message)
+
+
+def get_logger() -> logging.Logger:
+    """get_logger Method"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(list=PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Returns a connector to a MySQL database """
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+
+    cnx = mysql.connector.connection.MySQLConnection(user=username,
+                                                     password=password,
+                                                     host=host,
+                                                     database=db_name)
+    return cnx
+
+
+def main():
+    """
+    Obtain a database connection using get_db and retrieves all rows
+    in the users table and display each row under a filtered format
+    """
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    field_names = [i[0] for i in cursor.description]
+
+    logger = get_logger()
+
+    for row in cursor:
+        str_row = ''.join(f'{f}={str(r)}; ' for r, f in zip(row, field_names))
+        logger.info(str_row.strip())
+
+    cursor.close()
+    db.close()
 
 
 class RedactingFormatter(logging.Formatter):
@@ -32,61 +88,6 @@ class RedactingFormatter(logging.Formatter):
                             self.REDACTION,
                             Message,
                             self.SEPARATOR)
-
-
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    """
-        use a regex to replace occurrences of certain field values.
-    """
-    pattern = f"({'|'.join(fields)})=([^{separator}]*)"
-    return re.sub(pattern, lambda m: F"{m.group(1)}={redaction}", message)
-
-
-def get_logger() -> logging.Logger:
-    """get_logger Method"""
-    logger = logging.getLogger("user_data")
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-    stream_handler = logging.StreamHandler()
-    formatter = RedactingFormatter(fields=PII_FIELDS)
-    stream_handler.setFormatter(formatter)
-
-    logger.addHandler(stream_handler)
-    return logger
-
-
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """
-        function that returns a connector to the database
-    """
-    db_username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    db_password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
-    db_connection = mysql.connector.connection.MySQLConnection(
-        user=db_username,
-        password=db_password,
-        host=db_host,
-        database=db_name
-        )
-    return db_connection
-
-
-def main():
-    """
-        Main file
-    """
-
-    get_db = __import__('filtered_logger').get_db
-
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute("SELECT COUNT(*) FROM users;")
-    for row in cursor:
-        print(row[0])
-    cursor.close()
-    db.close()
 
 
 if __name__ == "__main__":
