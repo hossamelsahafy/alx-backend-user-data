a61a98084c8df17fd66486353df48a52eb445be7
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-05 00:29:54 +0300

    File

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,3 +14,11 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+        validate that the provided password
+        matches the hashed password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
