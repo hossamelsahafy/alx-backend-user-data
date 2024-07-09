3432241c0d5f18858939f4b657a4c9ad6ffabb12
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-10 01:42:12 +0300

    Files

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,14 @@ class Auth():
     """Define Auth Class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Return False And Path """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for ex_path in excluded_paths:
+            if path.rstrip('/') == ex_path.rstrip('/'):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
