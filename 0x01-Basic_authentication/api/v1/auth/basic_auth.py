60a333ebc2bfbeec5555021228003eb38247ca8f
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-10 03:45:08 +0300

    File

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Auth """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -18,3 +19,20 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(self,
+                                           base64_auth_header: str) -> str:
+        """
+             returns the decoded value
+             of a Base64 string base64_authorization_header
+        """
+        if base64_auth_header is None:
+            return None
+        if not isinstance(base64_auth_header, str):
+            return None
+        try:
+            decode_byte = base64.b64decode(base64_auth_header,
+                                           validate=True)
+            return decode_byte.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
