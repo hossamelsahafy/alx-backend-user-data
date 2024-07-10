a48c00b00465ee44fb49b681b133cc5549393d06
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-10 04:13:36 +0300

    File

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -62,7 +62,7 @@ class BasicAuth(Auth):
         """
         if user_email is None or not isinstance(user_email, str):
             return None
-        if user_pwd is None or not isinstance(user_pwd, str):
+        elif user_pwd is None or not isinstance(user_pwd, str):
             return None
         users = User.search({'email': user_email})
         if not users:
