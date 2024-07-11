a8007b8e4754b1bc64877e247bc8467834b9f8dc
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-11 22:00:42 +0300

    Files

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -3,7 +3,6 @@
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
 
-
 class SessionDBAuth(SessionExpAuth):
     """Define SessionDBAuth Class"""
     def __init__(self):
@@ -14,7 +13,7 @@ class SessionDBAuth(SessionExpAuth):
         """Create Session Method"""
         session_id = super().create_session(user_id)
         if session_id:
-            user = UserSession(user_id, session_id=session_id)
+            user_session = UserSession(user_id=user_id, session_id=session_id)
             return session_id
         return None
 
@@ -22,13 +21,13 @@ class SessionDBAuth(SessionExpAuth):
         """User ID for Session ID Method"""
         if session_id is None:
             return None
-        user = UserSession.query.filter_by(session_id=session_id).first()
-        if user:
-            return user.user_id
+        user_session = UserSession.query.filter_by(session_id=session_id).first()
+        if user_session:
+            return user_session.user_id
         return None
 
     def destroy_session(self, request=None):
         """Destroy Session Method"""
         if request and 'session_id' in request.cookies:
             session_id = request.cookies.get('session_id')
-            self.user_id_by_session_id(session_id, None)
+            self.user_id_by_session_id.pop(session_id, None)
