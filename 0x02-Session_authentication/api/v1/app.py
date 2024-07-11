6f867d2a54f2be3dcfd45e33a832053de241611f
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-11 18:28:51 +0300

    Files

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -47,23 +47,27 @@ def statuscode(error) -> str:
     return jsonify({"error": "Forbidden"}), 403
 
 
+request_path_list = [
+    '/api/v1/status',
+    '/api/v1/unauthorized/',
+    '/api/v1/forbidden/',
+    '/api/v1/auth_session/login/'
+]
+
+
 @app.before_request
 def before_request():
     """Before request handler"""
     if auth is None:
         return
 
-    request_path_list = [
-        '/api/v1/status',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/',
-        '/api/v1/auth_session/login/']
     if auth.require_auth(request.path, request_path_list):
         cookie = auth.session_cookie(request)
-        if auth.authorization_header(request) is None and cookie is None:
-            abort(401, description="Unauthorized")
+        if auth.authorization_header(request) is None or cookie is None:
+            abort(401)
         if auth.current_user(request) is None:
-            abort(403, description="Forbidden")
+            abort(403)
+        request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
