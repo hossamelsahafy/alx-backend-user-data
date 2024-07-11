83b7cf25e765d7527dafa2477bb4bcb8b5ba2d13
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-11 18:38:02 +0300

    Files

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -47,33 +47,26 @@ def statuscode(error) -> str:
     return jsonify({"error": "Forbidden"}), 403
 
 
-@ app.before_request
-def before_request() -> str:
-    """ Before Request Handler
-    Requests Validation
-    """
+@app.before_request
+def before_request():
+    """Before request handler"""
     if auth is None:
         return
 
-    excluded_paths = [
-        '/api/v1/status/',
+    request_path_list = [
+        '/api/v1/status',
         '/api/v1/unauthorized/',
         '/api/v1/forbidden/',
-        '/api/v1/auth_session/login/'
-    ]
-
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-
-    if auth.authorization_header(request) is None \
-            and auth.session_cookie(request) is None:
-        abort(401)
-
-    current_user = auth.current_user(request)
-    if current_user is None:
-        abort(403)
-
-    request.current_user = current_user
+        '/api/v1/auth_session/login']
+    
+    if auth:
+        if auth.require_auth(request.path, request_path_list):
+            cookie = auth.session_cookie(request)
+            if auth.authorization_header(request) is None and cookie is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
+            request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
