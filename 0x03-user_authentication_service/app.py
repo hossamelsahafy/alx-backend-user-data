c7ecf68b18d67bbc118b4e9d80f3a615e41d870d
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-17 22:56:00 +0300

    File

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -16,16 +16,15 @@ def basic() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route('/users', methods=['POST'])
+@app.route("/users", methods=["POST"], strict_slashes=False)
 def users() -> str:
-    """Create User"""
-    email = request.form.get('email')
-    password = request.form.get('password')
-    if not email or not password:
-        return jsonify({"message": "email or password missing"}), 400
+    """
+        Register a new user
+    """
+    email, password = request.form.get("email"), request.form.get("password")
     try:
         AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"}), 201
+        return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
