9e2423bc9fb83895b0fd1da845a58227f20af4e8
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-17 22:26:56 +0300

    File

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -13,7 +13,7 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def _hash_password(self, password: str) -> bytes:
+    def _hash_password(password: str) -> bytes:
         """Hash Password"""
         salt = bcrypt.gensalt()
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
