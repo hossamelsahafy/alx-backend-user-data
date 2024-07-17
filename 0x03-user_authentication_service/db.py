1ef35950fd9e5b0c0b83c9bb876daaddb894fe7d
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-17 21:56:29 +0300

    File

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -53,9 +53,20 @@ class DB:
             raise e
 
     def update_user(self, user_id: int, **kwargs) -> None:
+        """
+            Updates a user based on id
+        """
         user = self.find_user_by(id=user_id)
+        if user is None:
+            return
+        update_source = {}
         for key, value in kwargs.items():
-            if not hasattr(user, key):
-                raise ValueError
-            setattr(user, key, value)
+            if hasattr(User, key):
+                update_source[getattr(User, key)] = value
+            else:
+                raise ValueError()
+        self._session.query(User).filter(User.id == user_id).update(
+            update_source,
+            synchronize_session=False,
+        )
         self._session.commit()
