15a0a3d87601fb846698fad68afc076e86d88c0f
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-17 23:04:02 +0300

    File

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -11,6 +12,11 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+def _generate_uuid() -> str:
+    """Generate a new uuid"""
+    return str(uuid.uuid4())
 
 
 class Auth:
