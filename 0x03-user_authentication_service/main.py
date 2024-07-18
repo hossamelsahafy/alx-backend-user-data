e1b0d7b90cb6f448ae8ec9df17f2df3e7ae66a1b
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-18 19:53:25 +0300

    File

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -2,14 +2,62 @@
 """
 Main file
 """
+from auth import Auth
 
-from db import DB
-from user import User
 
-my_db = DB()
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
 
-user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
-print(user_1.id)
 
-user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
-print(user_2.id)
+def register_user(email: str, password: str) -> None:
+    """Register a user"""
+    return None
+
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    """Wrong password handling"""
+    pass
+
+
+def log_in(email: str, password: str) -> str:
+    """Login"""
+    pass
+
+
+def profile_unlogged() -> None:
+    """Progile Unlogged"""
+    pass
+
+
+def profile_logged(session_id: str) -> None:
+    """Profile Logged"""
+    pass
+
+
+def log_out(session_id: str) -> None:
+    """Loged out"""
+    pass
+
+
+def reset_password_token(email: str) -> str:
+    """Reset password token"""
+    pass
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """Update Password"""
+    pass
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
