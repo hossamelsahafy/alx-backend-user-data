22c25394ab7134214064b65bd1b056ad9e75e212
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-18 18:23:58 +0300

    File

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -55,7 +55,7 @@ class Auth:
         except NoResultFound:
             return None
 
-    def get_user_from_session_id(self, session_id:str) -> User:
+    def get_user_from_session_id(self, session_id: str) -> User:
         """Get User From Session Id"""
         if session_id is None:
             return None
